7ef02252c4b97ee2a4a205aad81094bd08b157b7
Alice Brunel <alc.brunel@gmail.com>
2019-07-06 14:42:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,16 @@
 require 'pry'
 
 def countdown(number)
+  sleep(5)
   while number != 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    sleep(1)
+    second = 1
+    sleep(second)
   end
   return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(second)
-  
+    sleep(second)
 end
